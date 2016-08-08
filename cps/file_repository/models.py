0d715fabf152ec95ae959521384d24a5f277f032
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2016-08-08 13:25:04 +0430

    Fix typo

--- cps/file_repository/models.py
+++ cps/file_repository/models.py
@@ -6,6 +6,6 @@ from django.utils.translation import ugettext_lazy as _
 
 class FileModel(models.Model):
     file = models.FileField(verbose_name=_("file"))
-    #name = models.CharField(verbose_name=_("name"), max_length=256)
+    name = models.CharField(verbose_name=_("name"), max_length=256)
     upload_date = models.DateTimeField(verbose_name=_("upload date"), auto_now_add=True)
     description = models.TextField(verbose_name=_("description"), blank=True)
