fbcd4f6e3ec3927da3218229afed638216799b6f
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2016-08-30 11:06:55 +0430

    Added template folders to settings. Renamed staticfiles to assets and added it to settings

--- cps/cps/settings.py
+++ cps/cps/settings.py
@@ -80,7 +80,7 @@ ROOT_URLCONF = 'cps.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -156,6 +156,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets"),
+]
 
 try:
     from .local_settings import *
