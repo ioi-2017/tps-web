3e6a7e414afc5bd7b45ad307777ca4c1f1ad9fca
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2017-02-01 22:00:16 +0330

    Added default settings for judge

--- cps/cps/settings.py
+++ cps/cps/settings.py
@@ -212,3 +212,15 @@ LOGIN_REQUIRED_URLS_EXCEPTIONS = (
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
+
+JUDGE_DEFAULT_NAME = 'local_runner'
+
+JUDGE_HANDLERS = {
+    'local_runner': {
+        'class': 'judges.runner.Runner',
+        'parameters': {
+            'compile_time_limit': 30,
+            'compile_memory_limit': 1024,
+        }
+    }
+}
