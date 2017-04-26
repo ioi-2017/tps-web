9c4461d687011992f5c82f260e8d4b7194689a68
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2017-04-27 01:15:22 +0430

    Admin for user roles

--- cps/problems/admin.py
+++ cps/problems/admin.py
@@ -9,7 +9,6 @@ admin.site.register(Discussion)
 admin.site.register(Solution)
 admin.site.register(SolutionRun)
 admin.site.register(Grader)
-# admin.site.register(SourceFile)
 admin.site.register(ProblemBranch)
 admin.site.register(TestCase)
 
@@ -17,3 +16,21 @@ admin.site.register(TestCase)
 @admin.register(ProblemRole)
 class RoleAdmin(admin.ModelAdmin):
     list_display = ['name']
+
+
+# FIXME: Temporal action for granting access to all.
+# Should be removed when access UI is implemented
+def grant_default_access_to_all(modeladmin, request, queryset):
+    from accounts.models import User
+    for user in User.objects.all():
+        for problem in Problem.objects.all():
+            ProblemUserRole.objects.get_or_create(
+                user=user,
+                problem=problem,
+                defaults={'role': ProblemRole.objects.all().first()}
+            )
+
+
+@admin.register(ProblemUserRole)
+class ProblemUserRoleAdmin(admin.ModelAdmin):
+    actions = [grant_default_access_to_all]
