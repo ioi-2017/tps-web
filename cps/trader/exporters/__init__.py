f93437ea62d9ce9a6fa6b3f3c809f6c40d55f067
Kiarash Golezardi <kia.masster@yahoo.com>
2017-10-21 11:10:36 +0330

    Refactor export code (#93)

--- cps/trader/exporters/__init__.py
+++ cps/trader/exporters/__init__.py
@@ -1,5 +1,9 @@
 from trader.exporters.json import *
 
+AVAILABLE_EXPORTER_CLASSES = [
+    JSONExporter,
+]
+
 AVAILABLE_EXPORTERS = [
-    (loader, loader.short_name) for loader in [JSONExporter]
+    (loader, loader.short_name) for loader in AVAILABLE_EXPORTER_CLASSES
 ]
